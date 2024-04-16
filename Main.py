24fb39d775337134fbaacebf5b90fc98cd32f3a5
A-Infor <67929816+A-Infor@users.noreply.github.com>
2024-04-16 15:08:01 -0300

    Código duplicado entre "FitNeuralNetwork" e "ApplyTraining".
    Opção por repassar as variáveis como parâmetros entre as duas funções.
    As variáveis a seguir têm sido declaradas em uma função, descartadas, e declaradas de novo em outra função: trainData, testData, monthTrainData, monthTestData, split, trainDataForPrediction, trainDataTrueValues, testDataForPrediction, testDataTrueValues, trainMonthsForPrediction, trainMonthForPredictedValues, testMonthsForPrediction, testMonthForPredictedValues.

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 #Deve-se passar o caminho para o xlsx da região para qual o modelo deve ser TREINADO
 from NeuralNetwork.NeuralNetwork import ApplyTraining, FitNeuralNetwork
 
-model = FitNeuralNetwork('./Data/spei12_riopardodeminas.xlsx', "Rio Pardo")
+model, trainData, testData, monthTrainData, monthTestData, split, trainDataForPrediction, trainDataTrueValues, testDataForPrediction, testDataTrueValues, trainMonthsForPrediction, trainMonthForPredictedValues, testMonthsForPrediction, testMonthForPredictedValues = FitNeuralNetwork('./Data/spei12_riopardodeminas.xlsx', "Rio Pardo")
 
-#Deve-se passar o caminho para o xlsx, o nome da Região e o modelo treinado;
-ApplyTraining("./Data/spei12_FranciscoSá.xlsx", "Francisco Sá", model)
+#Deve-se passar o caminho para o xlsx, o nome da RegiÃ£o e o modelo treinado;
+ApplyTraining("./Data/spei12_FranciscoSÃ¡.xlsx", "Francisco SÃ¡", model, trainData, testData, monthTrainData, monthTestData, split, trainDataForPrediction, trainDataTrueValues, testDataForPrediction, testDataTrueValues, trainMonthsForPrediction, trainMonthForPredictedValues, testMonthsForPrediction, testMonthForPredictedValues)
 #ApplyTraining("./Data/spei12_GrãoMogol.xlsx", "Grão Mogol", model)
 #ApplyTraining("./Data/spei12_Josenopolis.xlsx", "Josenópolis", model)
 
