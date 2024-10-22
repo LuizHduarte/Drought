eac9e58a530632c0b2bff8c2858c7181c4a2d632
Anderson Lima <67929816+A-Infor@users.noreply.github.com>
2024-10-22 16:03:15 -0300

    (`FitNeuralNetwork` + `ApplyTraining`) → `UseNeuralNetwork`.
    Making the code far less repetitive.

--- Main.py
+++ Main.py
@@ -1,11 +1,9 @@
 #Deve-se passar o caminho para o xlsx da região para qual o modelo deve ser TREINADO
-from NeuralNetwork.NeuralNetwork import ApplyTraining, FitNeuralNetwork
+from NeuralNetwork.NeuralNetwork import UseNeuralNetwork
 
-model = FitNeuralNetwork('./Data/spei12_riopardodeminas.xlsx', "Rio Pardo")
+model = UseNeuralNetwork('./Data/spei12_riopardodeminas.xlsx', "Rio Pardo", training=True)
 
 #Deve-se passar o caminho para o xlsx, o nome da Região e o modelo treinado;
-ApplyTraining("./Data/spei12_FranciscoSá.xlsx", "Francisco Sá", model)
+UseNeuralNetwork("./Data/spei12_FranciscoSá.xlsx", "Francisco Sá", model, training=False)
 #ApplyTraining("./Data/spei12_GrãoMogol.xlsx", "Grão Mogol", model)
 #ApplyTraining("./Data/spei12_Josenopolis.xlsx", "Josenópolis", model)
-
-
